6c809507046e860e0a90e62b55eb3122b49b77cc
Ronen Giladov <roneng@mail.sapir.ac.il>
2024-02-11 18:02:28 +0200

    Forth Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,77 @@
+class main{
+     static final Object lock = new Object();
+     static boolean isT2 = false;
+     static boolean isT3 = false;
+     static boolean isT1 = true;
 
-public class Main {
-    public static void main (String args[]){
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                while (true) {
+                    synchronized (lock) {
+                        while (!isT1) {
+                            try {
+                                lock.wait();
+                            } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                    // code block A
+                    System.out.println("A");
+                    // code block A
+                    isT1 = false;
+                    isT2 = true;
+                    lock.notifyAll();
+                    }
+                }
+            }
+        });
 
+        Thread t2 = new Thread(new Runnable() {
+            public void run() {
+                while (true) {
+                    synchronized (lock) {
+                        while (!isT2) {
+                            try {
+                                lock.wait();
+                            } catch (InterruptedException e) {
+                                Thread.currentThread().interrupt();
+                            }
+                        }
+                        // code block B
+                        System.out.println("B");
+                        // code block B
+                        isT3 = true;
+                        lock.notifyAll();
+                    }
+                }
+            }
+        });
+        Thread t3 = new Thread(new Runnable() {
+            public void run() {
+                while (true) {
+                    synchronized (lock) {
+                        while (!isT3) {
+                            try {
+                                lock.wait();
+                            } catch (InterruptedException e) {
+                                Thread.currentThread().interrupt();
+                            }
+                        }
+                        isT2 = false;
+                        // code block C
+                        System.out.println("C");
+                        // code block C
+
+                        isT3 = false;
+                        isT1 = true;
+                        lock.notifyAll();
+                    }
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+        t3.start();
     }
 }
